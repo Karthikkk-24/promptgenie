804fd4e8501031ca5464be3cf1658044029e46a1
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-19 00:48:41 +0530

    Added api endpoints and updated notes

---
+++ app/api/users/route.js
@@ -0,0 +1,30 @@
+// Path to the route
+// http://localhost:3000/api/users
+
+export async function GET(request) {
+    return new Response('Hello, Next.js!')
+}
+
+export async function POST(request) {
+    return new Response('Hello, Next.js!')
+}
+
+export async function PUT(request) {
+    return new Response('Hello, Next.js!')
+}
+
+export async function PATCH(request) {
+    return new Response('Hello, Next.js!')
+}
+
+export async function DELETE(request) {
+    return new Response('Hello, Next.js!')
+}
+
+export async function HEAD(request) {
+    return new Response('Hello, Next.js!')
+}
+
+export async function OPTIONS(request) {
+    return new Response('Hello, Next.js!')
+}
