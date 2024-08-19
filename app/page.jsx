e5147dbc61f4bab00f8eec9f5ea15366a064689a
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-19 09:00:34 +0530

    Changes to page, layout and jsconfig.json

---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <section className="w-full flex flex-col items-center justify-center">
+            <h1 className="head_text text-center">
+                Discover & Share
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">
+                    AI-Powered Prompts
+                </span>
+            </h1>
+            <p className="desc text-center">
+                PromptGenie is an open-source AI prompting tool for modern
+                creative prompts.
+            </p>
+
+            {/* feeds */}
+        </section>
+    );
+}
