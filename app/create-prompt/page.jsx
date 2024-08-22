b708eae098faf79aa8743f5fe89a5ef3918f3c54
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-22 20:30:04 +0530

    Added form in create-prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,5 +1,25 @@
-import React from 'react';
+'use client';
+import Form from '@components/Form';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export default function page() {
-    return <div>Create Prompt</div>;
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: '',
+    });
+
+    const createPrompt = async (e) => {};
+
+    return (
+        <Form
+            type="Create"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={createPrompt}
+        />
+    );
 }
